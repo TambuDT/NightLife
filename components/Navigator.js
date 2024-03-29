22898e78ee94a5c3f97ee43be35ec6f6154d09bd
Davide Tamburrelli <58508206+TambuDT@users.noreply.github.com>
2024-03-29 13:07:25 +0100

    icona compass e location invertite, hanno più senso in questo modo

--- components/Navigator.js
+++ components/Navigator.js
@@ -11,8 +11,8 @@ const Tab = createBottomTabNavigator()
 export default function Navbar() {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false, tabBarStyle: { backgroundColor: '#1c1c1c',borderTopWidth: 0 }, tabBarActiveTintColor: '#fff'}}>
-      <Tab.Screen name='Explore' component={Explore} options={{ tabBarIcon: ({ color, size }) => (<Ionicon name='compass-outline' color={color} size={size} />) }}></Tab.Screen>
-      <Tab.Screen name='Search' component={Search} options={{ tabBarIcon: ({ color, size }) => (<Ionicon name='location-outline' color={color} size={size} />) }}></Tab.Screen>
+      <Tab.Screen name='Explore' component={Explore} options={{ tabBarIcon: ({ color, size }) => (<Ionicon name='location-outline' color={color} size={size} />) }}></Tab.Screen>
+      <Tab.Screen name='Search' component={Search} options={{ tabBarIcon: ({ color, size }) => (<Ionicon name='compass-outline' color={color} size={size} />) }}></Tab.Screen>
       <Tab.Screen name='Profile' component={Profile} options={{ tabBarIcon: ({ color, size }) => (<Ionicon name='happy-outline' color={color} size={size} />) }}></Tab.Screen>
     </Tab.Navigator>
   )
