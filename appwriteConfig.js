9102f53c551db6002c39124e4e0b4a5a09f0dda0
Davide Tamburrelli <58508206+TambuDT@users.noreply.github.com>
2024-10-17 11:46:07 +0200

    Update appwriteConfig.js

--- appwriteConfig.js
+++ appwriteConfig.js
@@ -4,7 +4,7 @@ const client = new Client();
 
 client
   .setEndpoint("http://192.168.1.125/v1")
-  .setProject("6566342ccc7653dafe81");
+  .setProject("");
 
 const account = new Account(client);
 
